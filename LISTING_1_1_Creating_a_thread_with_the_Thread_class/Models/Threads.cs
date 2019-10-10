375b72dd71a35ec7def4a789b814e26f30f217dd
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-10-10 11:27:34 -0300

    listing 1.1

---
+++ LISTING_1_1_Creating_a_thread_with_the_Thread_class/Models/Threads.cs
@@ -0,0 +1,20 @@
+using System.Threading;
+
+namespace LISTING_1_1_Creating_a_thread_with_the_Thread_class.Models
+{
+public class Threads 
+{
+
+ public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine( "ThreadProc:  {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+
+}
+
+}
--- LISTING_1_1_Creating_a_thread_with_the_Thread_class/Program.cs
+++ LISTING_1_1_Creating_a_thread_with_the_Thread_class/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading;
+using LISTING_1_1_Creating_a_thread_with_the_Thread_class.Models;
 
 namespace LISTING_1_1_Creating_a_thread_with_the_Thread_class
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread t = new Thread(new ThreadStart(Threads.ThreadMethod));
+            t.Start();
+
+            for(int i = 0; i < 4; i++)
+            {
+                System.Console.WriteLine( "Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
         }
     }
 }
